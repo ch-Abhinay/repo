241f97329312bce452d4c20f74747209392f8a95
ch-Abhinay <120096746+ch-Abhinay@users.noreply.github.com>
2024-06-21 23:47:14 +0530

    removed readme and updated about

--- Desktop/cad abhi/react-trial/my-app/src/Mycomponents/About.js
+++ Desktop/cad abhi/react-trial/my-app/src/Mycomponents/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 function About() {
+  let style={
+    color:"blue",
+    fontSize:"30px",
+    textAlign:"center"
+  }
   return (
     <div>
       <h1>About Page</h1>
       <p>Welcome to the About page!</p>
+      <p style={style}>This is a basic para for testing</p>
     </div>
   );
 }
